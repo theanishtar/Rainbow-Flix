6ebd60c2e7eec1e80c6c0b63abc08865bcff06ae
Trần Hữu Đang <89396036+Theanishtar@users.noreply.github.com>
2023-08-18 23:34:32 +0700

    user router

--- server/models/login.js
+++
@@ -1,8 +0,0 @@
-var db = require('./database');
-exports.login = function (id, callback) {
-	let sql = 'SELECT * FROM account WHERE username = ?'
-	db.query(sql, id, (err, d) => {
-		data = d[0];
-		callback(data);
-	});
-} 
